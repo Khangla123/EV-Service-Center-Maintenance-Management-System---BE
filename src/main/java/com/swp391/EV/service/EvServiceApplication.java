112c3c26ae3ba9b0600e53e0f9ecadb9575a2e75
khangla123 <khanglvse182864@fpt.edu.vn>
2025-09-21 09:19:19 +0700

    first commit

---
+++ src/main/java/com/swp391/EV/service/EvServiceApplication.java
@@ -0,0 +1,13 @@
+package com.swp391.EV.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvServiceApplication.class, args);
+	}
+
+}
